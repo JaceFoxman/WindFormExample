28c9356e19ec56d27ba1a0f9ac4b9c03813c30d8
Jason Permann <JaceFoxman@gmail.com>
2025-03-03 08:34:00 -0700

    add exit and update buttons

--- WindFormExample/WindFormExampleForm.Designer.vb
+++ WindFormExample/WindFormExampleForm.Designer.vb
@@ -22,10 +22,43 @@ Partial Class WindFormExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.Update = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ExitButton.Location = New System.Drawing.Point(568, 332)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(220, 106)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'Update
+        '
+        Me.Update.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Update.Location = New System.Drawing.Point(12, 332)
+        Me.Update.Name = "Update"
+        Me.Update.Size = New System.Drawing.Size(220, 97)
+        Me.Update.TabIndex = 1
+        Me.Update.Text = "Update"
+        Me.Update.UseVisualStyleBackColor = False
+        '
+        'WindFormExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Update)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "WindFormExampleForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents Update As Button
 End Class
--- WindFormExample/WindFormExampleForm.vb
+++ WindFormExample/WindFormExampleForm.vb
@@ -1,3 +1,19 @@
-﻿Public Class WindFormExampleForm
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'Win Form Exaple
+'
+Option Strict On
+Option Explicit On
+Option Compare Text
 
+Public Class WindFormExampleForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Update_Click(sender As Object, e As EventArgs) Handles Update.Click
+        'Me.Text = "Forms Are Cool"
+        Me.Text = StrReverse(Me.Text)
+    End Sub
 End Class
